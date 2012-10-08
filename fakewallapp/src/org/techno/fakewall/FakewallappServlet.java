572f74bd5af78b6964a99208c662fe60a578d383
Bageshwar P Narain <hunnynarain@gmail.com>
2012-10-09 00:28:13 +0530

    Initializing GAE Project.

---
+++ fakewallapp/src/org/techno/fakewall/FakewallappServlet.java
@@ -0,0 +1,12 @@
+package org.techno.fakewall;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class FakewallappServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
