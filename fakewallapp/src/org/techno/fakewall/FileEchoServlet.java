28a1f7a3e4488221ee24d3712590154d7d4268d3
Bageshwar P Narain <hunnynarain@gmail.com>
2012-10-27 22:45:48 +0530

    Allowing only logged-in users to use FileEcho (upload)

--- fakewallapp/src/org/techno/fakewall/FileEchoServlet.java
+++ fakewallapp/src/org/techno/fakewall/FileEchoServlet.java
@@ -18,25 +18,30 @@ public class FileEchoServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 3495763991577008300L;
-	
+
 	private static final Logger logger = Logger.getLogger(FileEchoServlet.class.getName());
-	
-	private static final String OUTPUT="{\"success\":true,\"data\":\"data:image/png;base64,%s\"}";
 
-	public void doPost(HttpServletRequest request, HttpServletResponse resp) throws IOException {		
-		
-		InputStream reader = request.getInputStream();
-		byte data[]=new byte[64*1024]; //64K
-		String dec="";
-		
-		//THIS COULD fail for very large files.	
-		dec=Base64.encode(IOUtils.toByteArray(reader));
-		
-		logger.info("ECHO Message Length: "+dec.length());
-		
-		resp.setContentType("application/javascript");
-		resp.getWriter().write(String.format(OUTPUT, dec));
+	private static final String OUTPUT = "{\"success\":true,\"data\":\"data:image/png;base64,%s\"}";
 
+	public void doPost(HttpServletRequest request, HttpServletResponse resp) throws IOException {
+
+		Object access_token = request.getSession().getAttribute("access_token");
+		
+		//permit the upload service only to logged in users.
+		if (access_token != null) {
+			InputStream reader = request.getInputStream();
+			byte data[] = new byte[64 * 1024]; // 64K
+			String dec = "";
+
+			// THIS COULD fail for very large files.
+			dec = Base64.encode(IOUtils.toByteArray(reader));
+
+			logger.info("ECHO Message Length: " + dec.length());
+
+			resp.setContentType("application/javascript");
+			resp.getWriter().write(String.format(OUTPUT, dec));
+
+		}
 	}
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
